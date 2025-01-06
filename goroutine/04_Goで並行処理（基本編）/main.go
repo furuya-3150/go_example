ed84b7f5af24b15112dc08a39f4462d8188d4e48
taku <takumi.f.0308@icloud.com>
2025-01-06 23:18:10 +0900

    データ競合解決01

--- goroutine/04_Goで並行処理（基本編）/main.go
+++ goroutine/04_Goで並行処理（基本編）/main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	src := []int{1, 2, 3, 4, 5}
 	dst := []int{}
 
-	// 単一のデータに対して同時に読み書きを行うことで、データの一貫が取れなくなる
-	// この例ではdst
+	c := make(chan int)
+
 	for _, s := range src {
-		go func(s int) {
-
+		go func(s int, c chan int) {
 			result := s * 2
-
-			dst = append(dst, result)
-		}(s)
+			c <- result
+		}(s, c)
 	}
 
-	time.Sleep(time.Second)
+	for _ = range src {
+		num := <-c
+		dst = append(dst, num)
+	}
+
 	fmt.Println(dst)
+	close(c)
 }
