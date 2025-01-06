09a971c1fc6dcb48c507a3f1f13fbfb6030ddbc8
taku <takumi.f.0308@icloud.com>
2025-01-06 23:13:46 +0900

    データ競合

--- goroutine/04_Goで並行処理（基本編）/main.go
+++ goroutine/04_Goで並行処理（基本編）/main.go
@@ -6,11 +6,20 @@ import (
 )
 
 func main() {
-	for i := 0; i < 3; i++ {
-    go func(i int) {
-			// iをコピーして上書きされないよう修正
-			fmt.Println(i)
-    }(i)
+	src := []int{1, 2, 3, 4, 5}
+	dst := []int{}
+
+	// 単一のデータに対して同時に読み書きを行うことで、データの一貫が取れなくなる
+	// この例ではdst
+	for _, s := range src {
+		go func(s int) {
+
+			result := s * 2
+
+			dst = append(dst, result)
+		}(s)
 	}
-	time.Sleep(time.Second * 1)
+
+	time.Sleep(time.Second)
+	fmt.Println(dst)
 }
