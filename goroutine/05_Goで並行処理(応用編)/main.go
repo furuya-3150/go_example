8795f04cee26ce6acffefe65a4cf2e7717600047
taku <takumi.f.0308@icloud.com>
2025-01-07 00:09:44 +0900

    time.Afterでtimeout

--- goroutine/05_Goで並行処理(応用編)/main.go
+++ goroutine/05_Goで並行処理(応用編)/main.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	ch1 := make(chan int)
+
+	go func() {
+		time.Sleep(4 * time.Second)
+		ch1 <- 1
+	}()
+
+	for {
+		select {
+		case s := <-ch1:
+			fmt.Println(s)
+		case <-time.After(1 * time.Second): // ch1が受信できないまま1秒で発動
+			fmt.Println("time out")
+			return
+		/*
+		// これがあると無限ループする
+		default:
+			fmt.Println("default")
+			time.Sleep(time.Millisecond * 100)
+		*/
+		}
+	}
 }
 
 func restFunc() <-chan int {
