a5fe8c3dd098b10de08ace3b0f4231828286abf4
taku <takumi.f.0308@icloud.com>
2025-01-20 23:55:02 +0900

    json.marshal

---
+++ go-json-tutorial/go.mod
@@ -0,0 +1,3 @@
+module go-json-tutorial
+
+go 1.22.5
---
+++ go-json-tutorial/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Book struct {
+	Title string `json:"title"`
+	Author `json:"author"`
+}
+
+type Author struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+
+func main() {
+	fmt.Println("Hello World")
+
+	author := Author{Name: "hoge", Age: 12}
+	book := Book{Title: "Learning Concurrency in Python", Author: author}
+
+	fmt.Printf("%+v\n", book)
+
+	byteArray, err := json.MarshalIndent(book, "", "	")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(byteArray))
+}
